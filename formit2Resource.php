33b2c816bf6a5fe4b9f2cc1d191ac5a8b41c9316
phantom <phantomphondler@gmail.com>
2024-11-07 11:14:32 +0200

    Update formit2Resource.php

--- formit2Resource.php
+++ formit2Resource.php
@@ -1,4 +1,63 @@
 <?php
+/*
+$doc = $modx->newObject('modResource');
+$doc->set('createdby', $modx->user->get('id'));
+ 
+$allFormFields = $hook->getValues(); 
+foreach ($allFormFields as $field=>$value)
+{
+   $doc->set($field, $value);
+}
+
+$doc->set('parent', 290);
+$doc->set('alias', time());
+$doc->set('template', '5'); 
+$doc->set('published', '1');
+$doc->set('pagetitle', $pagetitle);
+$doc->set('material', $materialid);
+$doc->set('userreview', $userid);
+$doc->set('rating_1', $rating1val);
+$doc->set('review_1', $txtrate1);
+$doc->set('rating_2', $rating1val);
+$doc->set('review_2', $txtrate1);
+$doc->set('rating_3', $rating1val);
+$doc->set('review_3', $txtrate1);
+$doc->set('rating_4', $rating1val);
+$doc->set('review_4', $txtrate1);
+$doc->set('rating_5', $rating1val);
+$doc->set('review_5', $txtrate1);
+$doc->set('rating_6', $rating1val);
+$doc->set('review_6', $txtrate1);
+$doc->set('rating_7', $rating1val);
+$doc->set('review_7', $txtrate1);
+$doc->set('rating_8', $rating1val);
+$doc->set('review_8', $txtrate1);
+$doc->save();
+ 
+foreach ($allFormFields as $field=>$value)
+{
+    if ($tv = $modx->getObject('modTemplateVar', array ('name'=>$field)))
+    {
+
+        if (is_array($value)) {
+            $featureInsert = array();
+            while (list($featureValue, $featureItem) = each($value)) {
+                $featureInsert[count($featureInsert)] = $featureItem;
+                }
+            $value = implode('||',$featureInsert);
+            }   
+            
+        $tv->setValue($doc->get('id'), $value);
+
+        $tv->save();
+    }
+}
+ 
+return true;
+
+*/
+
+
 $doc = $modx->newObject('modResource');
 $doc->set('createdby', $modx->user->get('id'));
 
@@ -6,6 +65,7 @@ $allFormFields = $hook->getValues();
 
 // Set core fields
 foreach ($allFormFields as $field => $value) {
+    // Assuming only core fields like pagetitle and content should be directly set on the resource
     if (in_array($field, ['pagetitle', 'content'])) {
         $doc->set($field, $value);
     }
@@ -17,65 +77,33 @@ $doc->set('alias', time());
 $doc->set('template', '5'); 
 $doc->set('published', '1');
 
-// Save the resource first to generate an ID
-if ($doc->save() === false) {
-    return $modx->log(modX::LOG_LEVEL_ERROR, 'Failed to save resource');
-}
-
-// Check if the resource ID is generated
-$resourceId = $doc->get('id');
-if (!$resourceId) {
-    return $modx->log(modX::LOG_LEVEL_ERROR, 'Resource ID not generated');
-}
-
-// Debug to ensure resource is saved before proceeding with TVs
-$modx->log(modX::LOG_LEVEL_INFO, 'Resource created with ID: ' . $resourceId);
+$doc->save(); // Save the resource first to generate an ID
 
 // Loop to save template variables (TVs)
 foreach ($allFormFields as $field => $value) {
+    // Ensure this is a TV and not a core field
     if ($tv = $modx->getObject('modTemplateVar', ['name' => $field])) {
+        // Handle checkbox/multi-select arrays
         if (is_array($value)) {
             $value = implode('||', $value);
         }
-        $tv->setValue($resourceId, $value);
+        $tv->setValue($doc->get('id'), $value); // Set the TV value for this resource ID
         $tv->save();
-        $modx->log(modX::LOG_LEVEL_INFO, "Set TV {$field} to {$value} on resource {$resourceId}");
-    } else {
-        $modx->log(modX::LOG_LEVEL_ERROR, "Template Variable {$field} not found");
     }
 }
 
 // Manually set any other TV values directly if they aren't in $allFormFields
+/*
 $templateVars = [
-    'material' => $materialid,
-    'userreview' => $userid,
-    'rating_1' => $rating1val,
-    'review_1' => $txtrate1,
-    'rating_2' => $rating2val,
-    'review_2' => $txtrate2,
-    'rating_3' => $rating3val,
-    'review_3' => $txtrate3,
-    'rating_4' => $rating4val,
-    'review_4' => $txtrate4,
-    'rating_5' => $rating5val,
-    'review_5' => $txtrate5,
-    'rating_6' => $rating6val,
-    'review_6' => $txtrate6,
-    'rating_7' => $rating7val,
-    'review_7' => $txtrate7,
-    'rating_8' => $rating8val,
-    'review_8' => $txtrate8
-];
+    'material' => $materialid
+    
+];*/
 
 foreach ($templateVars as $tvName => $tvValue) {
     if ($tv = $modx->getObject('modTemplateVar', ['name' => $tvName])) {
-        $tv->setValue($resourceId, $tvValue);
+        $tv->setValue($doc->get('id'), $tvValue);
         $tv->save();
-        $modx->log(modX::LOG_LEVEL_INFO, "Manually set TV {$tvName} to {$tvValue} on resource {$resourceId}");
-    } else {
-        $modx->log(modX::LOG_LEVEL_ERROR, "Template Variable {$tvName} not found for manual setting");
     }
 }
 
 return true;
-?>
