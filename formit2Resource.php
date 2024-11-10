93dfb456bae089ff3b396db5ea34bbafa5140608
phantom <phantomphondler@gmail.com>
2024-11-10 17:11:29 +0200

    Update formit2Resource.php
    few fixes

--- formit2Resource.php
+++ formit2Resource.php
@@ -1,55 +1,103 @@
 <?php
-/*
-quick hook for formit. save as a snippet and call in formit hook
-make sure you input field correspond to the TV and fieldnames in the resource
-there might be security issues here that still need to be sorted
-*/
 
+$modx->setLogLevel(modX::LOG_LEVEL_ERROR);
+$modx->setLogTarget('FILE');
+
+$defaultParentId=290;
+$defaultPublished=0;
+$defaultTemplate=2;
+$defaultUser=732;
+
+// Load the Tagger service
+$tagger = $modx->getService('tagger'); // Simplified loading
+if (!$tagger) {
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Could not load Tagger service.');
+    return false; // This may also be unnecessary if the service is guaranteed to be available
+}
+
+// Create new resource
 $doc = $modx->newObject('modResource');
-$doc->set('createdby', $modx->user->get('id'));
+$userId = $modx->user->get('id') ? $modx->user->get('id') : $defaultUser;
+$doc->set('createdby', $userId);
 
-$allFormFields = $hook->getValues(); 
+$allFormFields = $hook->getValues();
 
 // Set core fields
 foreach ($allFormFields as $field => $value) {
-    // Assuming only core fields like pagetitle and content should be directly set on the resource
     if (in_array($field, ['pagetitle', 'content'])) {
         $doc->set($field, $value);
     }
 }
 
 // Set other resource properties
-$doc->set('parent', 31);
+$doc->set('parent', $defaultParentId); // Using integer value, which is suitable for IDs
 $doc->set('alias', time());
-$doc->set('template', '5'); 
-$doc->set('published', '1');
+$doc->set('template', $defaultTemplate); 
+$doc->set('published', $defaultPublished);
 
-$doc->save(); // Save the resource first to generate an ID
+if (!$doc->save()) {
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Failed to save resource');
+    return false;
+}
+
+$resourceId = $doc->get('id');
+$modx->log(modX::LOG_LEVEL_ERROR, "Resource saved with ID: $resourceId");
 
 // Loop to save template variables (TVs)
 foreach ($allFormFields as $field => $value) {
-    // Ensure this is a TV and not a core field
     if ($tv = $modx->getObject('modTemplateVar', ['name' => $field])) {
-        // Handle checkbox/multi-select arrays
         if (is_array($value)) {
             $value = implode('||', $value);
         }
-        $tv->setValue($doc->get('id'), $value); // Set the TV value for this resource ID
-        $tv->save();
+        $tv->setValue($resourceId, $value);
+        if (!$tv->save()) {
+            $modx->log(modX::LOG_LEVEL_ERROR, "Failed to save TV: $field for Resource ID: $resourceId");
+        }
     }
 }
 
-// Manually set any other TV values directly if they aren't in $allFormFields
-/*
-$templateVars = [
-    'material' => $materialid
-    
-];*/
+// Add tags using Tagger for each tag type
+$tagFields = [
+    'subject_tags' => 'subject_tagger_group_id',
+    'topic_tags' => 'topic_tagger_group_id',
+    'grade_tags' => 'grade_tagger_group_id',
+    'material_type_tags' => 'material_type_tagger_group_id',
+    'editor_tags' => 'editor_tagger_group_id',
+    'language_tags' => 'language_tagger_group_id',
+];
 
-foreach ($templateVars as $tvName => $tvValue) {
-    if ($tv = $modx->getObject('modTemplateVar', ['name' => $tvName])) {
-        $tv->setValue($doc->get('id'), $tvValue);
-        $tv->save();
+foreach ($tagFields as $tagField => $groupField) {
+    if (!empty($allFormFields[$tagField]) && !empty($allFormFields[$groupField])) {
+        $tags = explode(',', $allFormFields[$tagField]);
+        $taggerGroupId = (int) $allFormFields[$groupField];
+
+        $modx->log(modX::LOG_LEVEL_ERROR, "Processing tags for $tagField in group $taggerGroupId");
+
+        foreach ($tags as $tag) {
+            $tag = trim($tag);
+            if (!empty($tag)) {
+                $tagObject = $modx->getObject('TaggerTag', [
+                    'tag' => $tag,
+                    'group' => $taggerGroupId,
+                ]);
+
+                if (!$tagObject) {
+                    $tagObject = $modx->newObject('TaggerTag');
+                    $tagObject->set('tag', $tag);
+                    $tagObject->set('group', $taggerGroupId);
+                    if (!$tagObject->save()) {
+                        $modx->log(modX::LOG_LEVEL_ERROR, "Failed to create Tag: $tag in Group: $taggerGroupId");
+                    }
+                }
+
+                // Link the tag to the resource
+                if (!$tagObject->addMany($doc)) {
+                    $modx->log(modX::LOG_LEVEL_ERROR, "Failed to link Tag: $tag to Resource ID: $resourceId");
+                } else {
+                    $modx->log(modX::LOG_LEVEL_ERROR, "Linked Tag: $tag to Resource ID: $resourceId");
+                }
+            }
+        }
     }
 }
 
